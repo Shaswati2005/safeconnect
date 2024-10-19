cffc1c628737d6c69d957268875b5177d7c70659
dasbidyendu <150836749+Bidyendu221@users.noreply.github.com>
2024-10-19 14:10:33 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import Button from './Button'
-import Link from 'next/link'
-import { NAV_LINKS } from '@/constants'
+import React from "react";
+import Button from "./Button";
+import Link from "next/link";
+import { NAV_LINKS } from "@/constants";
 
 const Navbar = () => {
   return (
-   <nav className=' bg-orange-20 lg:rounded-bl-5xl  lg:rounded-br-5xl flexBetween max-container padding-container z-20  relative py-5'>
-    <Link href="/">
-    <h3 className='capitalise font-bold-18 text-skin-20 '>safe connect</h3>
-    </Link>
+    <nav className=" bg-orange-20 lg:rounded-bl-5xl  lg:rounded-br-5xl flexBetween max-container px-6 z-20  relative py-5">
+      <Link href="/">
+        <h3 className="capitalise font-bold-18 text-skin-20 ">safe connect</h3>
+      </Link>
 
-    <ul className="hidden h-full gap-15 lg:flex gap-20">
+      <ul className="hidden h-full gap-15 lg:flex gap-20">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-red-600 flex-center cursor-pointer pb-1 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-          {NAV_LINKS.map((link)=>(
-            <Link href={link.href} key={link.key} className="regular-16 text-red-600 flex-center cursor-pointer pb-1 transition-all hover:font-bold">
-              {link.label}
-            </Link>
-            
-          ))}
-          </ul>
-
-   </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
