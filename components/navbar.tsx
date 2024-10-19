e2db87a19a39b06ad9db274faad1b52337455210
Shaswati2005 <mishrashaswati2005@gmail.com>
2024-10-19 12:15:29 +0530

    added components

---
+++ components/navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from './Button'
+import Link from 'next/link'
+import { NAV_LINKS } from '@/constants'
+
+const Navbar = () => {
+  return (
+   <nav className='border-2 border-red-400 lg:border-rounded-2xl flexBetween max-container padding-container z-20  relative py-3'>
+    <Link href="/">
+    <h3 className='capitalise font-bold-18 text-skin-20 '>safe connect</h3>
+    </Link>
+
+    <ul className="hidden h-full gap-15 lg:flex gap-20">
+
+          {NAV_LINKS.map((link)=>(
+            <Link href={link.href} key={link.key} className="regular-16 text-skin-20 flex-center cursor-pointer pb-1 transition-all hover:font-bold">
+              {link.label}
+            </Link>
+            
+          ))}</ul>
+
+   </nav>
+  )
+}
+
+export default Navbar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,37 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+           brown:{
+            10:'#A77370',
+            20:'#C87377',
+            30:'#D38F8F',
+
+           },
+
+           orange:{
+            10:'#F39883',
+            20:'#F5AC95'
+
+           },
+
+           skin:{
+            10:'#FEE6C4',
+            20:'#FDE2CB',
+            30:'#FFBE98',
+           }
+
+
+      },
+      screens: {
+        xs: '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
       },
     },
   },
