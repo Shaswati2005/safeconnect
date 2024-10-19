bec52e26f9c5c5b575a03996fbff9c52512866c7
Shaswati2005 <mishrashaswati2005@gmail.com>
2024-10-19 12:21:31 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
+    <h1>hiii</h1>
     </>
   );
 }
