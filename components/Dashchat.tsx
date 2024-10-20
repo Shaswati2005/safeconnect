2dd48c73fce56856f8c651efe5a0eebc7e881194
Shaswati2005 <mishrashaswati2005@gmail.com>
2024-10-20 16:45:37 +0530

    anhnsvdhnasd

--- components/Dashchat.tsx
+++ components/Dashchat.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Dashchat = () => {
   return (
-    <section className='w-[350px] lg:w-[500px] h-full '>
-        
+    <section className='w-[350px] lg:w-[500px] h-full border-2 rounded-3xl border-violet-10 bg-violet-10 '>
+        <div className='flex flex-col items-center justify-start'>
+            <h1 className=''>your AI friend</h1>
+
+        </div>
+
     </section>
   )
 }
